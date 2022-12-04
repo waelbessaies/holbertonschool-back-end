6df8c729a82760c089e0f6564f61256fac61cc4e
root <root@LAPTOP-I8GVERUB.localdomain>
2022-12-04 18:55:07 +0100

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""employee IDreturns information about his/her TODO list progress."""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    employee = int(argv[1])
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(employee)).json()
+    TODO = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(employee)).json()
+    task_done = []
+    for task in TODO:
+        if task.get('completed') is True:
+            task_done.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(task_done), len(TODO)))
+    for done in task_done:
+        print("\t {}".format(done))
